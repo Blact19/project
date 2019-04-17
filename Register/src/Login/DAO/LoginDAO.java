44ba2fcdd7320ec3c058460d80155ca3700b65b7
Blact19 <46961728+Blact19@users.noreply.github.com>
2019-04-17 15:57:35 +0530

    Add files via upload

---
+++ Register/src/Login/DAO/LoginDAO.java
@@ -0,0 +1,52 @@
+package Login.DAO;
+import java.sql.*;
+
+
+
+import Login.Bean.Login;
+
+import register.DAO.*;
+
+
+public class LoginDAO {
+	public String authenticateUser(Login loginBean)
+	 {
+	 
+	String userName = loginBean.getUserId(); //Keeping user entered values in temporary variables.
+	 String password = loginBean.getPassword();
+	 
+	Connection con = null;
+	 Statement statement = null;
+	 ResultSet resultSet = null;
+	 
+	String userNameDB = "";
+	 String passwordDB = "";
+	 
+	try
+	 {
+	 con = DBConnection.createConnection(); //establishing connection
+	 statement = con.createStatement(); //Statement is used to write queries. Read more about it.
+	 resultSet = statement.executeQuery("select userId,Password from patient_reg"); //Here table name is users and userName,password are columns. fetching all the records and storing in a resultSet.
+	 
+	while(resultSet.next()) // Until next row is present otherwise it return false
+	 {
+	  userNameDB = resultSet.getString("UserId"); //fetch the values present in database
+	  passwordDB = resultSet.getString("Password");
+	 
+	   if(userName.equals(userNameDB) && password.equals(passwordDB))
+	   {
+	      return "SUCCESS"; ////If the user entered values are already present in database, which means user has already registered so I will return SUCCESS message.
+	   }
+	 }
+	 }
+	 catch(SQLException e)
+	 {
+	 e.printStackTrace();
+	 }
+	 return "Invalid user credentials"; // Just returning appropriate message otherwise
+	 
+	 }
+}
+
+	                
+
